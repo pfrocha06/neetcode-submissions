158c90da8dae06071f46540ca90cc2b22099c267
Pedro Rocha <48072994+pfrocha06@users.noreply.github.com>
2026-06-16 20:07:43 +0100

    Add: buy-and-sell-crypto - submission-0

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-0.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int left = 0;
+        int maxProfit = 0;
+
+        for (int right = 1; right < prices.length; right ++){
+            int profit = prices[right] - prices[left];
+
+            if (profit > maxProfit){
+                maxProfit = profit;
+            }if (prices[right] < prices[left]){
+                left = right;
+            }
+        }
+
+        return maxProfit;
+    }
+}
